798915279c77d1cfee809b31c60234b462b9b30d
Varsha Rani <varsharani.r.07@gmail.com>
2026-02-20 16:14:51 +0530

    UC5 created

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,51 +1,19 @@
-public class OOPSBannerApp {
+public class OOPSBannerApp{
 
     public static void main(String[] args) {
 
-        String[] banner = new String[7];
+        // Inline declaration + initialization + population
+        String[] banner = {
+                String.join("   ", " ***** ", " ***** ", " ******", " ***** "),
+                String.join("   ", "*     *", "*     *", "*     *", "*      "),
+                String.join("   ", "*     *", "*     *", "*     *", "*      "),
+                String.join("   ", "*     *", "*     *", "****** ", " ***** "),
+                String.join("   ", "*     *", "*     *", "*      ", "      *"),
+                String.join("   ", "*     *", "*     *", "*      ", "      *"),
+                String.join("   ", " ***** ", " ***** ", "*      ", " ***** ")
+        };
 
-        banner[0] = String.join("   ",
-                " ***** ",
-                " ***** ",
-                " ******",
-                " ***** ");
-
-        banner[1] = String.join("   ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*      ");
-
-        banner[2] = String.join("   ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*      ");
-
-        banner[3] = String.join("   ",
-                "*     *",
-                "*     *",
-                "****** ",
-                " ***** ");
-
-        banner[4] = String.join("   ",
-                "*     *",
-                "*     *",
-                "*      ",
-                "      *");
-
-        banner[5] = String.join("   ",
-                "*     *",
-                "*     *",
-                "*      ",
-                "      *");
-
-        banner[6] = String.join("   ",
-                " ***** ",
-                " ***** ",
-                "*      ",
-                " ***** ");
-
+        // Enhanced for loop for clean traversal
         for (String line : banner) {
             System.out.println(line);
         }
